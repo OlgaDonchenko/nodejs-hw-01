a53c85ca097f9548756832513f7ed1a87dc000eb
OlgaDonchenko <n0931606045@gmail.com>
2024-06-11 12:39:06 +0300

    5 steps done

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,5 +1,17 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const countContacts = async () => {};
+export const countContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    const contacts = JSON.parse(data);
+    return contacts.length;
+  } catch (error) {
+    console.error('Error reading contacts:', error);
+    return 0;
+  }
+};
 
-console.log(await countContacts());
+countContacts()
+  .then((count) => console.log(count))
+  .catch((err) => console.error(err));
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,21 @@
+import fs from 'fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 import { PATH_DB } from '../constants/contacts.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    const contacts = JSON.parse(data);
+
+    for (let i = 0; i < number; i++) {
+      const newContact = createFakeContact();
+      contacts.push(newContact);
+    }
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf8');
+    console.log(`Successfully added ${number} contacts.`);
+  } catch (error) {
+    console.error('Error generating contacts:', error);
+  }
+};
 
 generateContacts(5);
