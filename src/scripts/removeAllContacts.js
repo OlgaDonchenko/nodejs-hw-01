9f5cdeaa3d13f500664eb11bc3c17f929700d2b8
OlgaDonchenko <n0931606045@gmail.com>
2024-06-11 13:14:39 +0300

    all steps done

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,14 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  try {
+    const contacts = [];
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf8');
+    console.log('All contacts have been removed.');
+  } catch (error) {
+    console.error('Error removing contacts:', error);
+  }
+};
 
 removeAllContacts();
