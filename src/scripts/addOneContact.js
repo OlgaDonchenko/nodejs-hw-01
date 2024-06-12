ab04876f3f709e6c80b4edba6b38c28b32870c7d
OlgaDonchenko <n0931606045@gmail.com>
2024-06-12 13:53:25 +0300

    fixed mist

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -4,12 +4,15 @@ import { PATH_DB } from '../constants/contacts.js';
 
 export const addOneContact = async () => {
   try {
-    const data = await fs.readFile(PATH_DB, 'utf8');
-    const contacts = JSON.parse(data);
+    const contacts = await fs.readFile(PATH_DB, 'utf8');
     const newContact = createFakeContact();
-    contacts.push(newContact);
-    await fs.writeFile(PATH_DB, JSON.stringify(newContact, null, 2), 'utf8');
-    console.log('Successfully added one contact.');
+    const updatedContacts = [...JSON.parse(contacts), newContact];
+    await fs.writeFile(
+      PATH_DB,
+      JSON.stringify(updatedContacts, null, 2),
+      'utf8',
+    );
+    console.log('Successfully added one contact to the file.');
   } catch (error) {
     console.error('Error adding contact:', error);
   }
